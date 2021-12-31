cba870eba9857913687561f95cce4440eb803e18
yuririn <yuri.soushi@gmail.com>
2021-12-31 20:40:26 +0800

    headerを装飾
    styled-components、スタイルを追加

--- src/components/header.js
+++ src/components/header.js
@@ -2,37 +2,78 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 import { siteMetadata } from "../../gatsby-config"
+import styled from "styled-components" //追加
 
 const Header = ({ location }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
   let siteName
   if (isRootPath) {
-    siteName = <h1>{siteMetadata.title}</h1>
+    siteName = <h1 className="logo">{siteMetadata.title}</h1>
   } else {
     siteName = (
-      <p>
+      <p className="logo">
         <Link to={rootPath}>{siteMetadata.title}</Link>
       </p>
     )
   }
   return (
-    <header>
-      {siteName}
-      <nav>
-        <ul>
-          <li>
-            <Link to="/blogs/">ブログ</Link>
-          </li>
-          <li>
-            <Link to="/about/">About Me</Link>
-          </li>
-          <li>
-            <Link to="/contact/">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <HeaderWrapper>
+      <div className="container">
+        {siteName}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/blogs/">ブログ</Link>
+            </li>
+            <li>
+              <Link to="/about/">About Me</Link>
+            </li>
+            <li>
+              <Link to="/contact/">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </HeaderWrapper>
   )
 }
 export default Header
+
+const HeaderWrapper = styled.header`
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+
+  .container {
+    height: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .logo {
+    font-size: 18px;
+    margin: 0;
+    font-weight: bold;
+
+    a {
+      text-decoration: none;
+      color: var(--black);
+    }
+  }
+
+  nav ul {
+    margin: 0;
+    list-style: none;
+    display: flex;
+
+    li {
+      padding: 0 0 0 20px;
+
+      a {
+        text-decoration: none;
+        color: var(--black);
+        font-weight: bold;
+      }
+    }
+  }
+`
