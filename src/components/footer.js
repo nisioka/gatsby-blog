6e04eaee36821df41a23d8ed0baa29e28654410e
yuririn <yuri.soushi@gmail.com>
2021-12-31 20:42:21 +0800

    footer 装飾
    センタリングのみ

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,19 @@
 import * as React from "react"
 
 import { siteMetadata } from "../../gatsby-config"
+import styled from "styled-components"
 
 const Footer = () => {
   return (
-    <footer>
+    <FooterWrapper>
       <p>
         <small>(c) 2021 {siteMetadata.title}</small>
       </p>
-    </footer>
+    </FooterWrapper>
   )
 }
 export default Footer
+
+const FooterWrapper = styled.footer`
+  text-align: center;
+`
